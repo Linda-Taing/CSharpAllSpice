b3c73aa5c4f7d7b8dd7b31a7cc7002c631b3132f
Linda Taing <lindatdoan@me.com>
2023-03-22 15:33:38 -0600

    Finally logged in and connected

---
+++ CSharpAllSpice.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'linda-taing.us.auth0.com'
+export const clientId = 'KJLmJ9da3J86wMJjWqTxIiwpKno4YP03'
+export const audience = 'https://bcw-example.com'
