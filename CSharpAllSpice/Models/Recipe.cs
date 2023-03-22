837c16b509efe4859b926b27085e5aacd1a837c0
Linda Taing <lindatdoan@me.com>
2023-03-22 16:27:59 -0600

    Created Table and Added a Value

---
+++ CSharpAllSpice/Models/Recipe.cs
@@ -0,0 +1,7 @@
+namespace CSharpAllSpice.Models
+{
+    public class Recipe
+    {
+
+    }
+}
--- CSharpAllSpice/dbSetup.sql
+++ CSharpAllSpice/dbSetup.sql
@@ -7,3 +7,23 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS recipes(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  instructions VARCHAR(500),
+  imgUrl VARCHAR(500),
+  category VARCHAR(255),
+  creatorId VARCHAR(50),
+
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+)default charset utf8 COMMENT '';
+
+ALTER TABLE recipes
+
+DROP Table recipes;
+
+INSERT INTO recipes 
+(title, instructions, imgUrl, category, creatorId)
+VALUES
+('Buttered Toast', 'Get 2 slices of bread. Pop them in the toaster. Get your butter as much or as little as you like... Toast Pops and you can spread the butter on to the warm delicious toast. Now simply enjoy.', 'https://media.istockphoto.com/id/172413173/photo/toast-and-butter.jpg?b=1&s=170667a&w=0&k=20&c=KDDcmP7ezM7HmWjZfCmZDjCRGDEBVE359_YiI7T4bUQ=', 'Breakfast', '640538132afd6827951b3197')
