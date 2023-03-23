6a82d89958d13ff686ec003e77a9e8ce7f3381d9
Linda Taing <lindatdoan@me.com>
2023-03-23 12:21:10 -0600

    POSTMAN TEST WORKS FOR create recipes and Ingerdients and gets

--- CSharpAllSpice/Models/Ingredient.cs
+++ CSharpAllSpice/Models/Ingredient.cs
@@ -4,7 +4,7 @@ public class Ingredient
 {
     public int Id { get; set; }
     public string CreatorId { get; set; }
-    public string RecipeId { get; set; }
+    public int RecipeId { get; set; }
     public string Name { get; set; }
     public string Quantity { get; set; }
 
--- CSharpAllSpice/dbSetup.sql
+++ CSharpAllSpice/dbSetup.sql
@@ -30,7 +30,7 @@ VALUES
 
 SELECT
 rec.*,
-acct.*
+acct.name AS creatorName
 FROM recipes rec
 JOIN accounts acct ON rec.creatorId = acct.id;
 
@@ -39,7 +39,7 @@ JOIN accounts acct ON rec.creatorId = acct.id;
 
 CREATE TABLE IF NOT EXISTS ingredients(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  creatorId VARCHAR(50) NOT,
+  creatorId VARCHAR(50) NOT NULL,
   recipeId INT NOT NULL,
   name VARCHAR(50),
   quantity VARCHAR(100), 
