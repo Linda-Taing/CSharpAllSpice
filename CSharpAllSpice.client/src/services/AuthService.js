fcd8e7b9522bbc2f908a2f7bf005806597b3b3da
Linda Taing <lindatdoan@me.com>
2023-04-11 22:54:17 -0600

    c

--- CSharpAllSpice.client/src/services/AuthService.js
+++ CSharpAllSpice.client/src/services/AuthService.js
@@ -28,7 +28,7 @@ AuthService.on(AuthService.AUTH_EVENTS.AUTHENTICATED, async function () {
   await accountService.getAccount()
   socketService.authenticate(AuthService.bearer)
   // NOTE if there is something you want to do once the user is authenticated, place that here
-  // Didn't know how to draw it to page but could see them in the console.
+  // This is to have the function run at login 
   // await favoritesService.getMyFavorites()
 })
 
